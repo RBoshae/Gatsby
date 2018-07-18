0047cb74f00646c2cc113c7477ecc919156e27d4
Wanderrer <rwboshae@gmail.com>
2018-07-18 03:06:23 -0700

    Completed Part One andt One and Two

---
+++ tutorial-part-one/src/pages/page-3.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+  <div>
+    <h1>Welcome to page 3</h1>
+    <p>You made it!</p>
+    <Link to="/">back home</Link>
+  </div>
+)
